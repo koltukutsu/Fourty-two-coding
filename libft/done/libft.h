7730d96b0f6414a255366ca78e54b82e6a3be3e9
koltukustu-huawei <semih.babacan@std.yildiz.edu.tr>
2022-03-07 02:03:49 +0300

    ft_atio is added

--- libft/done/libft.h
+++ libft/done/libft.h
@@ -13,6 +13,7 @@ void *ft_memcpy(void *dest, const void *src, size_t n);
 void *ft_memset(void *s, int c, size_t n);
 void ft_bzero(void *s, size_t n);
 int ft_strncmp(const char *s1, const char *s2, size_t n);
+int ft_atoi(const char *nptr);
 int ft_isalpha(int c);
 int ft_isdigit(int c);
 int ft_isalpha(int c);
