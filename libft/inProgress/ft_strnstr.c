352291d5043420824d251ac54d700e58e0bb9b59
koltukustu-zbook <semih.babacan@std.yildiz.edu.tr>
2022-03-07 20:20:25 +0300

    last commit

---
+++ libft/inProgress/ft_strnstr.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+char *strnstr(const char *big, const char *little, size_t len)
+{
+    if (ft_strlen(little) == 0)
+    {
+        return (big);
+    }
+
+    char c;
+    char sc;
+
+    while (len--)
+    {
+        if(*ptr_big++ ==
+    }
+}
