d65323f2a4c9c335f28d37df44886a27e6dff0a9
koltukustu-zbook <semih.babacan@std.yildiz.edu.tr>
2022-03-07 20:36:19 +0300

    since do while is forbidden, I modified the code

--- libft/done/ft_atoi.c
+++ libft/done/ft_atoi.c
@@ -4,7 +4,7 @@ static int ft_multiply(int digit, int decimal_value)
     int total;
 
     total = digit - 48;
-    do
+    while (decimal_value-- != -1);
     {
         if (decimal_value == 0)
         {
@@ -14,7 +14,7 @@ static int ft_multiply(int digit, int decimal_value)
         {
             total = total * 10;
         }
-    } while (decimal_value--);
+    } 
 
     return total;
 }
